fa4270aa8372df1f9a539e071619ecdc3473f425
Lok <jc_bart18@yahoo.com>
2023-06-07 03:44:08 +0800

    initial commit

---
+++ Epon/MainActivity.cs
@@ -0,0 +1,79 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+
+namespace Epon
+{
+    
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        EditText editText1, editText2;
+        Button btn1, btn2, btn3;
+        TextView savingsView, targetView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            editText1 = FindViewById<EditText>(Resource.Id.editText1);
+            editText2 = FindViewById<EditText>(Resource.Id.editText2);
+            btn1 = FindViewById<Button>(Resource.Id.btn1);
+            btn2 = FindViewById<Button>(Resource.Id.btn2);
+            btn3 = FindViewById<Button>(Resource.Id.btn3);
+            savingsView = FindViewById<TextView>(Resource.Id.savingsView);
+            targetView = FindViewById<TextView>(Resource.Id.targetView);
+            btn1.Click += btn1_Click;
+            btn2.Click += btn2_Click;
+            btn3.Click += btn3_Click;
+
+          
+            
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = Double.Parse(editText1.Text);
+                double b = Double.Parse(savingsView.Text);
+
+                savingsView.Text = (a + b).ToString();
+                Toast.MakeText(this, "Savings added!", ToastLength.Short).Show();
+               
+            }
+            catch (Exception)
+            {
+                Toast.MakeText(this, "'Please input value first!", ToastLength.Short).Show();
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = Double.Parse(editText1.Text);
+                double b = Double.Parse(savingsView.Text);
+            
+            savingsView.Text = ((a - b) *-1).ToString();
+            Toast.MakeText(this, "Savings deducted!", ToastLength.Short).Show();
+        }
+            catch (Exception)
+            {
+                Toast.MakeText(this, "'Please input value first!", ToastLength.Short).Show();
+            }
+        }
+        
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            Toast.MakeText(this, "target money!", ToastLength.Short).Show();
+            targetView.Text = editText2.Text;
+            editText2.Text = "";
+        }
+
+    }
+}
---
+++ Epon/obj/Debug/80/android/src/md526d325e34ef975b2e9961d8bbd65d5d4/loadActivity.java
@@ -0,0 +1,47 @@
+package md526d325e34ef975b2e9961d8bbd65d5d4;
+
+
+public class loadActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Epon.loadActivity, Epon", loadActivity.class, __md_methods);
+	}
+
+
+	public loadActivity ()
+	{
+		super ();
+		if (getClass () == loadActivity.class)
+			mono.android.TypeManager.Activate ("Epon.loadActivity, Epon", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
